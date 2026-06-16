fa7f2c5fa3991cad5362d5d958135f6bc52116b3
Rama Prasad Sahu <ramaprasadsahu008@gmail.com>
2026-06-16 17:18:53 +0530

    utils files added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-import { app } from './app.js'
+import app  from './app.js'
 import dns from "dns"
 
 dns.setServers(['8.8.8.8', '1.1.1.1'])
@@ -15,7 +15,7 @@ dotenv.config({
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`)
     })
 })
 .catch((err) => {
