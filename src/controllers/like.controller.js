74e2d01c24ffad5cdecdf05ef455ac638e7341f2
Rama Prasad Sahu <ramaprasadsahu008@gmail.com>
2026-07-16 17:52:17 +0530

    enhance like controller: add validation and error handling for video and comment likes

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -7,12 +7,64 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, " InValid ObjectID")
+    }
+    const video = await Video.findById(videoId)
+    if(!video){
+        throw new ApiError(404 , " Video Not Found")
+    }
+    const Existinglike = await Like.findOne({
+        video : videoId,
+        likedBy : req.user?._id
+    })
+    if(Existinglike){
+        await Like.findByIdAndDelete(Existinglike._id)
+        
+        return res
+            .status(200)
+            .json(new ApiResponse(200 , {isliked : false} , "Video Like Removed SuccessFully"))
+    }
+    await Like.create({
+        video : videoId,
+        likedBy : req.user?._id
+    })
+    return res 
+            .status(200)
+            .json(new ApiResponse(200,{isliked : true }, " Video Liked Successfully"))
+
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(400 , "Invalid Object ID") 
+    }
+    const comment = await Comment.findById(commentId)
+    if(!comment){
+        throw new ApiError(404 , " Comment Like Not Found")
+    }
+    const Existinglike = await Like.findOne({
+        comment : commentId,
+        likedBy : req.user?._id
+    })
+    if(Existinglike){
+        await Like.findByIdAndDelete(Existinglike._id)
+        return res
+            .status(200)
+            .json(new ApiResponse(200 , {isliked : false} , "Comment like deleted Successfully"))
 
+        }
+         await Like.create({
+            comment : commentId,
+            likedBy : req.user?._id
+         })
+    return res
+            .status(200)
+            .json(new ApiResponse(200 , {isliked : true} , "Comment like Added Successfully"))
+   
+    
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
